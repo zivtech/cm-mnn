94d590e3791bb3dc2580c234c3051164556337da
Mark <libkuman@cm.mnn.org>
2018-06-09 00:14:34 +0000

    another round of development

--- sites/all/modules/custom/mnn_qnap_show/mnn_qnap_show.js
+++ sites/all/modules/custom/mnn_qnap_show/mnn_qnap_show.js
@@ -4,35 +4,45 @@
       ////////////////////////////////////////////////////////////////////////
       //ON READY FUNCTION
       $(document).ready(function() {
-	console.log('hi mom');
-	$('#mnn_cm_xml_wrapper').hide();
+	var airing =  $('#edit-mnn-cm-airing').val();
+
+	if (airing > 0) {
+	  mnnQnapShowMatcherSelects();
+	}
+	else {
+	  $('#mnn_cm_xml_wrapper').hide();
+	}
       });
       ////////////////////////////////////////////////////////////////////////
       //CHANGE FUNCTION FOR VOLUNTEER TYPE FIELD
       $("select[id$='edit-mnn-cm-airing']", context).
 	change(function() {
-	  console.log('hi dad');
 	  mnnQnapShowMatcherSelects();
 	});
 
       ////////////////////////////////////////////////////////////////////////
-      //FUNCTION WILL HANDLE INTERACTIONS BETWEEN POSITION AND OPPORTUNITY TYPE
+      //FUNCTION WILL HANDLE INTERACTIONS BETWEEN AIRDATE AND XML FILENAME
       function mnnQnapShowMatcherSelects() {
 	var $xml_file_select = $('#edit-mnn-cm-xml-files');
 	var airing =  $('#edit-mnn-cm-airing').val();
 
-	console.log('airing: ' + airing);
-
 	if (airing > 0) {
 	  var cm_url = '/show/xml/?airing_id=' + airing;
+	  var exact_match = false;
 	  console.log(cm_url);
 	  //REMOVE CURRENT OPTIONS, REPLACE WITH OPTIONS FROM AJAX CALL
 	  $xml_file_select.empty();
 	  $.getJSON(cm_url, function(data){
 	    $.each(data, function(i,item){
-	      console.log(item.id);
-	      console.log(item.label);
-	      if (item.id==666) {
+	      if (item.id == 'exact') {
+		exact_match = true;
+		$xml_file_select.append($("<option></option>")
+					.attr("value", item.id)
+					.text(item.label));
+		
+	      }
+	      else if (Boolean(exact_match)) {
+		exact_match = false;
 		$xml_file_select.append($("<option></option>")
 				   .attr("value", item.id)
 				   .attr('selected', true)
